d4187ddabe9755c9c1d2c63f993acde8a491b09e
Yulia Mamaeva <yulia.mamaeva00@gmail.com>
2024-02-06 19:57:28 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            'warn'
+        ]
+    }
+};
